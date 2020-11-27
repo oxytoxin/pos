6266168cb37d197af43979e55f78feaea2fb9d20
oxytoxin <oxytoxinsgrace@gmail.com>
2020-11-27 17:56:33 +0800

    barcode scanner test

--- app/Http/Livewire/Pos/Index.php
+++ app/Http/Livewire/Pos/Index.php
@@ -72,59 +72,59 @@ class Index extends Component
 
     public function addViaBarcode()
     {
-        if (strlen($this->query) == 13) {
-            $product = Product::where('barcode', $this->query)->first();
-            if ($product) {
-                if (!$this->sale) {
-                    $this->sale = Sale::create([
-                        'reference_no' => time() . auth()->user()->phone,
-                        'customer_id' => $this->customer_id,
-                        'cashier_id' => auth()->user()->cashier->id,
-                    ]);
-                }
-                if ($this->sale->products->contains($product)) {
-                    $this->sale->products->find($product)->pivot->update([
-                        'quantity' => $this->sale->products->find($product)->pivot->quantity + 1,
-                    ]);
-                } else $this->sale->products()->attach($product->id);
-                $this->sale = Sale::with('products')->find($this->sale->id);
-                $this->query = "";
-                $this->dispatchBrowserEvent('success', ['message' => 'Product added!']);
-            } else {
-                $this->dispatchBrowserEvent('warning', ['message' => 'Product not found.']);
-                $this->query = "";
-                return false;
+        // if (strlen($this->query) == 13) {
+        $product = Product::where('barcode', $this->query)->first();
+        if ($product) {
+            if (!$this->sale) {
+                $this->sale = Sale::create([
+                    'reference_no' => time() . auth()->user()->phone,
+                    'customer_id' => $this->customer_id,
+                    'cashier_id' => auth()->user()->cashier->id,
+                ]);
             }
+            if ($this->sale->products->contains($product)) {
+                $this->sale->products->find($product)->pivot->update([
+                    'quantity' => $this->sale->products->find($product)->pivot->quantity + 1,
+                ]);
+            } else $this->sale->products()->attach($product->id);
+            $this->sale = Sale::with('products')->find($this->sale->id);
+            $this->query = "";
+            $this->dispatchBrowserEvent('success', ['message' => 'Product added!']);
         } else {
-            $this->dispatchBrowserEvent('error', ['message' => 'Invalid barcode!']);
+            $this->dispatchBrowserEvent('warning', ['message' => 'Product not found.']);
             $this->query = "";
             return false;
         }
+        // } else {
+        //     $this->dispatchBrowserEvent('error', ['message' => 'Invalid barcode!']);
+        //     $this->query = "";
+        //     return false;
+        // }
     }
 
-    public function updatedQuery($value)
-    {
-        if (strlen($value) == 13) {
-            $product = Product::where('barcode', $this->query)->first();
-            if ($product) {
-                if (!$this->sale) {
-                    $this->sale = Sale::create([
-                        'reference_no' => time() . auth()->user()->phone,
-                        'customer_id' => $this->customer_id,
-                        'cashier_id' => auth()->user()->cashier->id,
-                    ]);
-                }
-                if ($this->sale->products->contains($product)) {
-                    $this->sale->products->find($product)->pivot->update([
-                        'quantity' => $this->sale->products->find($product)->pivot->quantity + 1,
-                    ]);
-                } else $this->sale->products()->attach($product->id);
-                $this->sale = Sale::with('products')->find($this->sale->id);
-                $this->query = "";
-                $this->dispatchBrowserEvent('success', ['message' => 'Product added!']);
-            }
-        }
-    }
+    // public function updatedQuery($value)
+    // {
+    //     if (strlen($value) == 13) {
+    //         $product = Product::where('barcode', $this->query)->first();
+    //         if ($product) {
+    //             if (!$this->sale) {
+    //                 $this->sale = Sale::create([
+    //                     'reference_no' => time() . auth()->user()->phone,
+    //                     'customer_id' => $this->customer_id,
+    //                     'cashier_id' => auth()->user()->cashier->id,
+    //                 ]);
+    //             }
+    //             if ($this->sale->products->contains($product)) {
+    //                 $this->sale->products->find($product)->pivot->update([
+    //                     'quantity' => $this->sale->products->find($product)->pivot->quantity + 1,
+    //                 ]);
+    //             } else $this->sale->products()->attach($product->id);
+    //             $this->sale = Sale::with('products')->find($this->sale->id);
+    //             $this->query = "";
+    //             $this->dispatchBrowserEvent('success', ['message' => 'Product added!']);
+    //         }
+    //     }
+    // }
 
     public function addProduct(Product $product)
     {
